f15ff815ddcf54e1bc3a8c39a6ca24bd5f5c82b8
nabara99 <rtaufiq212@gmail.com>
2023-07-16 20:37:30 +0800

    order & payment-callback

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,60 @@
-'use strict';
+"use strict";
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const result = await super.create(ctx);
+
+    console.log("result", result);
+
+    const midtransClient = require("midtrans-client");
+    // Create Snap API instance
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: "SB-Mid-server-SpHrgbHhjRVWqge0Bdsz5whJ",
+      clientKey: "SB-Mid-client-yiYed4C5aPpNoWBh",
+    });
+
+    let parameter = {
+      transaction_details: {
+        order_id: result.data.id,
+        gross_amount: result.data.attributes.totalPrice,
+      },
+      credit_card: {
+        secure: true,
+      },
+    };
+
+    let response = await snap.createTransaction(parameter);
+
+    // const midtransClient = require("midtrans-client");
+    // // Create Core API instance
+    // let core = new midtransClient.CoreApi({
+    //   isProduction: false,
+    //   serverKey: "SB-Mid-server-SpHrgbHhjRVWqge0Bdsz5whJ",
+    //   clientKey: "SB-Mid-client-yiYed4C5aPpNoWBh",
+    // });
+
+    // let parameter = {
+    //   payment_type: "gopay",
+    //   transaction_details: {
+    //     gross_amount: result.data.attributes.totalPrice,
+    //     order_id: result.data.id,
+    //   },
+    //   credit_card: {
+    //     token_id: "CREDIT_CARD_TOKEN", // change with your card token
+    //     authentication: true,
+    //   },
+    // };
+
+    // // charge transaction
+    // let response = await core.charge(parameter);
+
+    return response;
+  },
+}));
